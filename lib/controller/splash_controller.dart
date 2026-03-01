d71f9b1c68949fec487135d93a3d3f436e1a6e75
khan <shahzaibkhan3356@gmail.com>
2026-03-01 15:11:27 +0500

    feat: implement app initialization and splash transition logic

---
+++ lib/controller/splash_controller.dart
@@ -0,0 +1,16 @@
+import 'package:get/get.dart';
+
+import '../core/routes/app_routes.dart';
+
+class SplashController extends GetxController {
+  @override
+  void onInit() {
+    super.onInit();
+    _navigateToHome();
+  }
+
+  void _navigateToHome() async {
+    await Future.delayed(const Duration(seconds: 3));
+    Get.offAllNamed(RouteNames.main);
+  }
+}
